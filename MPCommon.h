f28df0fbdcd93d4cac3daea22b5fb7e2cd1a34c7
qingyixiaoxia <lwjyqq@163.com>
2017-12-29 22:40:58 +0800

    ADD SOURCE FILE

---
+++ MPCommon.h
@@ -0,0 +1,44 @@
+/* Copyright Wenjing Li. and other Node contributors. All rights reserved.
+* Author: Wenjing Li, GitHub Address: http://www.github.com/qingyixiaoxia
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to
+* deal in the Software without restriction, including without limitation the
+* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+* sell copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*/
+
+#ifndef MULTI_PLATFORM_COMMON_H
+#define MULTI_PLATFORM_COMMON_H
+
+#ifndef NULL
+#define NULL   0
+#endif
+#ifndef IN
+#define IN
+#endif
+#ifndef OUT
+#define OUT
+#endif
+#ifndef INOUT
+#define INOUT
+#endif
+
+typedef enum
+{
+    MP_BASE_LIBRARY_LINUX = 1,
+	MP_BASE_LIBRARY_WIN32 = 2,
+} MPBaseLibraryType;
+#endif
